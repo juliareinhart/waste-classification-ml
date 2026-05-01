bd92f5a14b2c345d06a97c4545cb75d3060e0c24
Carlos Cazares <cgcazares1@csuchico.edu>
2026-05-01 00:15:35 -0700

    Main Code
    the full Xcode project folder.
    The yolo-export Jupyter notebook

---
+++ iPhone-YOLOv8/RealWasteDetector/RealWasteDetector/RealWasteDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealWasteDetectorApp.swift
+//  RealWasteDetector
+//
+//  Created by Car on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct RealWasteDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
